ead10436ab7e666655efd13764b4cf4a74c7c4aa
YUD96 <yudai000916@gmail.com>
2020-09-05 16:47:30 +0900

    until fix google oauth

--- src/main.js
+++ src/main.js
@@ -2,11 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBP4m9TOMOlsNYa1SRhf8ufDaEuRRu7j7Q",
+  authDomain: "my-address-pj-91fae.firebaseapp.com",
+  databaseURL: "https://my-address-pj-91fae.firebaseio.com",
+  projectId: "my-address-pj-91fae",
+  storageBucket: "my-address-pj-91fae.appspot.com",
+  messagingSenderId: "63260257335",
+  appId: "1:63260257335:web:83c0ad4aed683bbf5623b2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
